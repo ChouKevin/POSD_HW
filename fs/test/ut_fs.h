f81c0b6d822014ead4751de9d27e7315a347fc42
ChouKevin <uuuu12tw@gmail.com>
2018-10-28 12:18:34 +0900

    add unit test

--- fs/test/ut_fs.h
+++ fs/test/ut_fs.h
@@ -6,35 +6,48 @@
 #include "../src/file.h"
 #include "../src/visitor.h"
 #include <sstream>
-TEST (MiscTest, lstatOnFolder)
+TEST(MiscTest, lstatOnFolder)
 {
-  const char * path = "./test_data";
-  struct stat st;
-  ASSERT_EQ(0, lstat(path, &st));
-  // ASSERT_EQ(128, st.st_size);
-  ASSERT_EQ(true, S_ISDIR(st.st_mode));
-  ASSERT_EQ(false, S_ISREG(st.st_mode));
-  ASSERT_EQ(false, S_ISLNK(st.st_mode));
+    const char *path = "./test_data";
+    struct stat st;
+    ASSERT_EQ(0, lstat(path, &st));
+    // ASSERT_EQ(128, st.st_size);
+    ASSERT_EQ(true, S_ISDIR(st.st_mode));
+    ASSERT_EQ(false, S_ISREG(st.st_mode));
+    ASSERT_EQ(false, S_ISLNK(st.st_mode));
 }
 
-TEST (FileSystemTest, SIZE)
+TEST(FileSystemTest, SIZE)
 {
-  Node * test_data = new Folder ("./test_data");
-  ASSERT_EQ(512,test_data->size());
-  Node * folder = new Folder("./test_data/folder");
-  ASSERT_EQ(512, folder->size());
-  Node * hello_cpp = new File("./test_data/hello.cpp");
-  ASSERT_EQ(87, hello_cpp->size());
-  Node * a_out = new File("./test_data/folder/a.out");
-  ASSERT_EQ(8432, a_out->size());
-  test_data->add(folder);
-  test_data->add(hello_cpp);
-  ASSERT_EQ(2,test_data->numberOfChildren());
+    Node *test_data = new Folder("./test_data");
+    ASSERT_EQ(512, test_data->size());
+    Node *folder = new Folder("./test_data/folder");
+    ASSERT_EQ(512, folder->size());
+    Node *hello_cpp = new File("./test_data/hello.cpp");
+    ASSERT_EQ(87, hello_cpp->size());
+    Node *a_out = new File("./test_data/folder/a.out");
+    ASSERT_EQ(8432, a_out->size());
+    test_data->add(folder);
+    test_data->add(hello_cpp);
+    ASSERT_EQ(2, test_data->numberOfChildren());
 }
-TEST (PRACTICE, VISITOR){
-  File * a_out = new File("./test_data/folder/a.out");
-  SizeVisitor * icv = new SizeVisitor();
-  a_out->accept(icv);
-  ASSERT_EQ(8432, icv->getSize());
+TEST(PRACTICE, VisitFileSize)
+{
+    File *a_out = new File("./test_data/folder/a.out");
+    SizeVisitor *icv = new SizeVisitor();
+    a_out->accept(icv);
+    ASSERT_EQ(8432, icv->getSize());
 }
+TEST(PRACTICE, VisitFolderSize)
+{
+    SizeVisitor* icv = new SizeVisitor();
+    Node *folder = new Folder("./test_data/folder");
+    File *a_out = new File("./test_data/folder/a.out");
+    folder->accept(icv);
+    ASSERT_EQ(0, icv->getSize());
+    folder->add(a_out);
+    folder->accept(icv);
+    ASSERT_EQ(8432, icv->getSize());
+}
+
 #endif
